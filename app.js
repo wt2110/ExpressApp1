447c83ae48b716b87a2e37e8ed38c13518478f90
wt2110 <34961679+wt2110@users.noreply.github.com>
2019-03-20 14:52:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(function (err, req, res, next) {
     });
 });
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 80);
 
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
